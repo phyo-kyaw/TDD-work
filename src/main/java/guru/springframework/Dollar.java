7e6e0df9f3814c0c0571cf5079b9dbab2d1faa63
phyo-kyaw <phy0ky6w@gmail.com>
2021-03-14 00:45:06 +1100

    equality

---
+++ src/main/java/guru/springframework/Dollar.java
@@ -0,0 +1,16 @@
+package guru.springframework;
+
+public class Dollar extends Money {
+
+    public Dollar( int amount){
+
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier){
+       return new Dollar(amount * multiplier);
+    }
+
+
+
+}
