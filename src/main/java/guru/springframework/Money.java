a4baefca4d70c93a051c82fd2da7df97a390dcca
phyo-kyaw <phy0ky6w@gmail.com>
2021-03-14 15:47:36 +1100

    make It

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -47,6 +47,11 @@ public class Money implements Expression {
     }
 
     public Expression plus(Money addend){
-        return new Money(amount + addend.amount, this.currency);
+        return new Sum(this, addend);
+    }
+
+    @Override
+    public Money reduce(String to) {
+        return this;
     }
 }
